31ece65fe44cb63de12821e9a89500915836964f
Felipe Nascimento <56228311+felipejn@users.noreply.github.com>
2020-04-12 02:27:44 +0100

    Alteração chave server

--- seguranca/cadastrar.php
+++ seguranca/cadastrar.php
@@ -8,7 +8,7 @@ curl_setopt($ch, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify")
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
-	"secret"=>"6Lc-zegUAAAAAH1VfNQQzv7iq0DzqDsKX3bbfE5S",
+	"secret"=>"chave server",
 	"response"=>$_POST["g-recaptcha-response"],
 	"remoteip"=>$_SERVER["REMOTE_ADDR"] //-> Pega o ip do usuario
 )));
